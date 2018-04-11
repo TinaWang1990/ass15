876ea19461b1dabf6fd2598515a18214724c46ae
TinaWang <shihuiwang1990@gmail.com>
2018-04-11 18:08:06 +0000

    anwser

--- c.php
+++ c.php
@@ -2,20 +2,29 @@
 $username=$_POST['username'];
 $password=$_POST['password'];
 
-if($username==""&&$password==""){
-	$str='It is not completed!';
+if($username&&$password){
+	$c="root";
+	$d="root";
+	$response=array();
+
+	if($username=$c&&$password=$d){
+		$response=array(
+			"res"=>"success"
+		);
+	}else{
+		$response=array(
+			"res"=>"failed"
+		);
+	}
+	echo json_encode($response);
 }else{
-	$str='It is completed successful!';
+//why??
 }
 
 header("Access-Control_Allow-Origin:*");
 header("Content-Type: application/json; charset=UTF-8");
 
-$response=array(
-	"message"=>$str,
-	
-);
-echo json_encode($response);
+
 
 
 
--- demo_get_json.php
+++ demo_get_json.php
@@ -11,20 +11,3 @@
 // );
 // echo json_encode($response);
 
-$username=$_POST['username'];
-$password=$_POST['password'];
-
-if($username==""&&$password==""){
-	$str='It is not completed!';
-}else{
-	$str='It is completed successful!';
-}
-
-header("Access-Control_Allow-Origin:*");
-header("Content-Type: application/json; charset=UTF-8");
-
-$response=array(
-	"message"=>$str,
-	
-);
-echo json_encode($response);
